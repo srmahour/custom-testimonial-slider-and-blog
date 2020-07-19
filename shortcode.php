2a5a019155501e8e04922936484316d1d14d7f4b
Sandeep Mahaur <sadeepmahour@gmail.com>
2020-07-19 14:46:41 +0530

    Update shortcode.php
    changes

--- shortcode.php
+++ shortcode.php
@@ -3,8 +3,9 @@ function testiminial_shortcode($attr){
 $testimonial_obj = new WP_Query(array('post_type' => 'testimonial', 'post_per_page' => $attr['limit']));
 	
 $testiVar = '';
-while($testimonial_obj->have_posts()): $testimonial_obj->the_post();
 $testiVar .= '<div class="testimonials">';
+while($testimonial_obj->have_posts()): $testimonial_obj->the_post();
+
 $testiVar .=	'<div class="testimonial">';
 $testiVar .=		'<div class="author-img">';
 $testiVar .=			'<img src="'.get_the_post_thumbnail_url(get_the_ID(),'thumbnail').'" alt="" class="img-fluid rounded">';
@@ -14,8 +15,9 @@ $testiVar .=			'<h4>'.get_the_title(get_the_ID()).'</h4>';
 $testiVar .=		'<blockquote> <i class="fa fa-quote-left"></i>'.get_the_content(get_the_ID()).'<i class="fa fa-quote-right"></i> </blockquote>';
 $testiVar .=		'</div>';
 $testiVar .=	'</div>';
-$testiVar .= '</div>';
+
 endwhile;
+$testiVar .= '</div>';
 return $testiVar;
 	
 }
